41ea43a615657ec089db91337ed514037e7e8eb7
Raoul Schwenke <raoul.schwenke@gmail.com>
2023-09-03 14:06:06 +0200

    V1

--- src/main/java/pie/empie/photos/Photo.java
+++ src/main/java/pie/empie/photos/Photo.java
@@ -1,5 +1,7 @@
 package pie.empie.photos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.validation.constraints.NotEmpty;
 
 public class Photo {
@@ -8,6 +10,9 @@ public class Photo {
 
     @NotEmpty
     private String fileName;
+
+    @JsonIgnore
+    private byte[] data;
 
     public Photo() {
     }
@@ -19,6 +24,14 @@ public class Photo {
 
     // Raw Data
 
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
 
     public String getId() {
         return id;
--- src/main/java/pie/empie/photos/PhotosController.java
+++ src/main/java/pie/empie/photos/PhotosController.java
@@ -2,27 +2,29 @@ package pie.empie.photos;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.*;
 
 @RestController
 public class PhotosController {
 
     private Map<String, Photo> db = new HashMap<>() {{
-       put("1", new Photo("1", "hello.jpg"));
+        put("1", new Photo("1", "hello.jpg"));
     }};
 
-        @GetMapping("/")
-        public String hello(){
-            return "Hello World";
-        }
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
 
-        @GetMapping("/photos")
-        public Collection<Photo> get() {
-            return db.values();
-        }
+    @GetMapping("/photos")
+    public Collection<Photo> get() {
+        return db.values();
+    }
 
     @GetMapping("/photos/{id}")
     public Photo get(@PathVariable String id) {
@@ -38,9 +40,11 @@ public class PhotosController {
     }
 
     @PostMapping("/photos")
-    public Photo create(@RequestBody @Valid Photo photo) {
-            photo.setId(UUID.randomUUID().toString());
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = new Photo();
+        photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(file.getOriginalFilename());
         db.put(photo.getId(), photo);
         return photo;
     }
-        }
+}
